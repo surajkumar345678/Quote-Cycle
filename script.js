72c66629416077bc21931a3e033aeb39bef4c5f2
Suraj Kumar <kumarsuraj345678@gmail.com>
2022-06-19 14:00:54 +0530

    manipulate api functions

--- script.js
+++ script.js
@@ -1,22 +1,45 @@
-let apiQuotes=[];
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+let apiQuotes = [];
 
 // show new quote
 function newQuote() {
     // select random quote
-    const quote=apiQuotes[Math.floor(Math.random() *apiQuotes.length)];
-    console.log(quote);
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    // empty author field
+    if (!quote.author) {
+        authorText.textContent = 'Unknown';
+    } else {
+        authorText.textContent = quote.author;
+    }
+
+    quoteText.textContent = quote.text;
 }
 
 // quotes api
-async function getQuotes(){
-    const apiUrl='https://type.fit/api/quotes';
+async function getQuotes() {
+    const apiUrl = 'https://type.fit/api/quotes';
     try {
         const response = await fetch(apiUrl);
-        apiQuotes=await response.json();
+        apiQuotes = await response.json();
         newQuote();
-    } catch(error){
-        
+    } catch (error) {
+
     }
 }
+
+// Tweet quote
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+// new quote button
+newQuoteBtn.addEventListener('click', newQuote);
+// tweetQuote button
+twitterBtn.addEventListener('click', tweetQuote);
 
 getQuotes();
